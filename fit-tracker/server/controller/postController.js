d525f9ad6616003515546d6192d196a9e6545bfb
Cameron Reed <cameronreedadjusting@gmail.com>
2020-03-26 10:58:31 -0700

    Adjusted postController file

--- fit-tracker/server/controller/postController.js
+++ fit-tracker/server/controller/postController.js
@@ -1,1 +1,26 @@
 const posts = require('../../db.json')
+
+module.exports = {
+    getPosts: (req, res, next) => {
+        res.status(200).send(posts)
+    },
+    addPost: (req, res, next) => {
+        const newPost = {
+            id: Num,
+            todo: req.body.post
+        }
+        posts.push(newPost)
+        num++
+        res.status(200).send(posts)
+    },
+    editPost: (req, res, next) => {
+        // write code that will edit a post
+        
+    },
+    deletePost: (req, res, next) => {
+        const index = posts.findIndex ((elem) => {
+            return elem.id == req.params.id
+        })
+        posts.splice(index, 1)
+    }
+}
