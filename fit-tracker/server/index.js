6b41fa852b91f39f547eb01f82157770b7928b3d
Cameron Reed <cameronreedadjusting@gmail.com>
2020-03-26 10:44:15 -0700

    Updated db.json and postController

---
+++ fit-tracker/server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const {getPosts, addPost, editPost, deletePost} = require('./controller/postController')
+
+const app = express()
+
+app.use(express.json())
+
+// GET
+app.get('/api/posts', getPosts)
+// POST
+app.post('/api/add_post', addPost)
+// PUT
+app.put('/api/edit_post', editPost)
+//DELETE
+app.delete('/api/delete_post', deletePost)
+
+// console.log(posts)
+
+app.listen(port, () => console.log(`listening on port ${port}`))
+
