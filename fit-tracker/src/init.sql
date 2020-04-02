b228fc2cfe35531086ed0bfe88e5be96a502a2fa
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-02 09:23:16 -0700

    made init.sql file

---
+++ fit-tracker/src/init.sql
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+/*[
+    {
+        "id": 1,
+        "post": "3/23 - I worked quads, calves and hamstrings, 3 sets each."
+        
+    },
+    {
+        "id": 2,
+        "post": "3/24 - I benched 3 sets and did 6 total sets of bicep/tricep supersets."
+        
+    },
+    {
+        "id": 3,
+        "post": "3/25 - I ran 1 mile today in 6 minutes 13 seconds."
+        
+    }
+]*/
